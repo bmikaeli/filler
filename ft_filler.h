b3d6d80a3754a70b22bb2f0d630bd4ea3128f05e
Baptiste MIKAELIAN <bmikaeli@e3r2p21.42.fr>
2014-01-21 19:42:43 +0100

    maj

--- ft_filler.h
+++ ft_filler.h
@@ -5,51 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmikaeli <bmikaeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/01/13 13:42:54 by bmikaeli          #+#    #+#             */
-/*   Updated: 2014/01/15 18:58:15 by bmikaeli         ###   ########.fr       */
+/*   Created: 2014/01/18 14:01:42 by bmikaeli          #+#    #+#             */
+/*   Updated: 2014/01/21 18:58:10 by bmikaeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_FILLER_H
 # define FT_FILLER_H
-# include <stdio.h>
-# include "libft/includes/libft.h"
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <unistd.h>
 
-typedef	struct	s_struct
+# include <unistd.h>
+# include "libft/includes/libft.h"
+
+typedef struct s_struct
 {
-	int		map_w;
-	int		map_h;
-	char	**tmpmap;
-	char	**tmppiece;
-	int		piece_h;
-	int		piece_w;
-	char	*p1;
-	char	*p2;
-	char	**tmpplayer;
-	char	map[100][100];
-	char	piece[10][10];
+	char	data[999][999];
+	int		weight;
+	int		high;
+	int		final_x;
+	int		final_y;
 	int		i;
 	int		j;
-	int		start_X_x;
-	int		start_X_y;
-	int		start_O_x;
-	int		start_O_y;
-	int		head_of_piece_x;
-	int		head_of_piece_y;
-	int		final_print_x;
-	int		final_print_y;
+	int		x;
+	int		y;
 }				t_struct;
-
-void	get_info(char *line, t_struct *coucou);
-void	get_full_map(char *line, t_struct *coucou);
-void	get_piece(t_struct *coucou, char *line);
-void	print_result(int x, int y);
-int		could_be_print_here(int x, int y, t_struct *coucou);
-int		where_to_print(t_struct *coucou);
-int		get_start_location(t_struct *coucou, char *line);
-
 #endif
